d7aca0a59bad8216955217fa9e5482434c9a442d
Sarukesh Boominathan <sarukesh.boominatha@student.tuke.sk>
2024-03-12 13:26:10 +0530

    chenaged logo 8:56 AM

--- Project1/backend/frontend/src/Header.js
+++ Project1/backend/frontend/src/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Header.css';
 import image from './logo.jpg';
+import image_png from './logo1.jpg';
 
 const Header = () => {
   return(
     <header>
     <div id='logo_pic'>
-      <img class="profile-in-about" src={image}
+      <img class="profile-in-about" src={image_png}
         alt="image"></img><br></br>
     
         <h1>SARUKESH BOOMINATHAN</h1>
--- Project1/backend/frontend/src/Navbar.js
+++ Project1/backend/frontend/src/Navbar.js
@@ -20,4 +20,5 @@ const Navbar = () => {
 
 }
 
+
 export default Navbar
